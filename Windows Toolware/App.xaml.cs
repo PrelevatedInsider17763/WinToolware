526267512f824baea0fe54498961bd93ea300075
Duddu, Sreeram Sathya <madnessmaddenwindows@prelevatedinsider.org>
2023-08-31 23:07:24 +0530

    with changes

--- Windows Toolware/App.xaml.cs
+++ Windows Toolware/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,26 @@ namespace Windows_Toolware
     /// </summary>
     public partial class App : Application
     {
+        private const int MINIMUM_SPLASH_TIME = 4500; // Miliseconds  
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Windows_Toolware.Splash.SplashScreen splash = new Windows_Toolware.Splash.SplashScreen();
+            splash.Show();
+            // Step 2 - Start a stop watch  
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            // Step 3 - Load your windows but don't show it yet  
+            base.OnStartup(e);
+            MainWindow main = new MainWindow();
+            timer.Stop();
+
+            int remainingTimeToShowSplash = MINIMUM_SPLASH_TIME - (int)timer.ElapsedMilliseconds;
+            if (remainingTimeToShowSplash > 0)
+                Thread.Sleep(remainingTimeToShowSplash);
+
+            splash.Close();
+        }
     }
 }
--- Windows Toolware/MainWindow.xaml.cs
+++ Windows Toolware/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Ookii.Dialogs.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -19,6 +20,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 using MessageBox = System.Windows.MessageBox;
+using System.Security.Principal;
+
 
 namespace Windows_Toolware
 {
@@ -30,38 +33,40 @@ namespace Windows_Toolware
 
     public partial class MainWindow : MetroWindow
     {
+
         public MainWindow()
         {
           
             Hide();
             CredentialDialog creddialog = new CredentialDialog();
                 // The window title will not be used on Vista and later; there the title will always be "Windows Security".
-                creddialog.WindowTitle = "Credential dialog sample";
-                creddialog.MainInstruction = "Microsoft Windows Toolware";
-                creddialog.Content = "Welcome to Windows Toolware. Please sign in to your account.";
+                creddialog.WindowTitle = "Ravenite";
+                creddialog.MainInstruction = "Prelevated Insider Ravenite Codename 'Mostar'";
+                creddialog.Content = "Welcome to Ravenite. Please sign in to your account.";
                 creddialog.ShowSaveCheckBox = true;
                 creddialog.ShowUIForSavedCredentials = true;
                 // The target is the key under which the credentials will be stored.
                 // It is recommended to set the target to something following the "Company_Application_Server" pattern.
                 // Targets are per user, not per application, so using such a pattern will ensure uniqueness.
-                creddialog.Target = "Microsoft.WindowsToolware_x64__CREDDIALOG.21387";
+                creddialog.Target = "PrelevatedInsider.Ravenite_x64__CREDDIALOG.21387";
                 if (creddialog.ShowDialog(this))
                 {
                     // Normally, you should verify if the credentials are correct before calling ConfirmCredentials.
                     // ConfirmCredentials will save the credentials if and only if the user checked the save checkbox.
                     creddialog.ConfirmCredentials(true);
                 }
+
             Show();
             InitializeComponent();
-            UsernameText.Text = "Hello, " + creddialog.Credentials.UserName;
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\sreer\source\repos\Windows Toolware\Windows Toolware\Sounds\Windows Toolware startup.wav");
-            player.Play();
+            UsernameText.Text = "Hello, " + creddialog.Credentials.UserName + "!";
+            //System.Media.SoundPlayer player = new System.Media.SoundPlayer(Directory.GetCurrentDirectory().ToString() + @"\Sounds\Windows Toolware startup.wav");
+            //player.Play();
         }
 
         public void WindowsExtendExpiryBuild()
         {
             ExpiryDate expiry = ExpiryDate.GetExpiryDate(20211031);
-            ExpiryDate expiry11 = ExpiryDate.GetExpiryDate(20220915);
+            ExpiryDate expiry11 = ExpiryDate.GetExpiryDate(20830720);
             string build = Environment.OSVersion.Version.Build.ToString();
             if (build == expiry)
             {
@@ -80,26 +85,26 @@ namespace Windows_Toolware
             using (CredentialDialog dialog = new CredentialDialog())
             {
                 // The window title will not be used on Vista and later; there the title will always be "Windows Security".
-                dialog.WindowTitle = "Credential dialog sample";
-                dialog.MainInstruction = "Microsoft Windows Toolware";
-                dialog.Content = "Welcome to Windows Toolware. Please sign in to your account.";
+                dialog.WindowTitle = "Ravenite";
+                dialog.MainInstruction = "Prelevated Insider Ravenite Codename 'Mostar'";
+                dialog.Content = "Welcome to Ravenite. Please sign in to your account.";
                 dialog.ShowSaveCheckBox = true;
                 dialog.ShowUIForSavedCredentials = true;
                 // The target is the key under which the credentials will be stored.
                 // It is recommended to set the target to something following the "Company_Application_Server" pattern.
                 // Targets are per user, not per application, so using such a pattern will ensure uniqueness.
-                dialog.Target = "Microsoft.WindowsToolware_x64__CREDDIALOG.21387";
+                dialog.Target = "PrelevatedInsider.Ravenite_x64__CREDDIALOG.21387";
                 if (dialog.ShowDialog(this))
                 {
                     // Normally, you should verify if the credentials are correct before calling ConfirmCredentials.
                     // ConfirmCredentials will save the credentials if and only if the user checked the save checkbox.
                     dialog.ConfirmCredentials(true);
-                    UsernameText.Text = "Hello, " + dialog.Credentials.UserName;
+                    UsernameText.Text = "Hello, " + dialog.Credentials.UserName + "!";
                 }
             }
             Show();
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\sreer\source\repos\Windows Toolware\Windows Toolware\Sounds\Windows Toolware startup.wav");
-            player.Play();
+            //System.Media.SoundPlayer player = new System.Media.SoundPlayer(Directory.GetCurrentDirectory().ToString() + @"\Sounds\Windows Toolware startup.wav");
+            //player.Play();
         }
 
         private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
@@ -130,5 +135,6 @@ namespace Windows_Toolware
         {
             
         }
+
     }
 }
